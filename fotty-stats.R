81f800f55129c1dc7b3157d1f7d401ee8f2c4041
whitesgsw <seanwhitehead@live.com.au>
2024-04-03 09:42:05 +0800

    updated model

--- fotty-stats.R
+++ fotty-stats.R
@@ -3,7 +3,7 @@ library(fitzRoy)
 library(readr)
 
 # create a list of integers for past 5 years
-years <- seq(2019, 2023, 1)
+years <- seq(2017, 2023, 1)
 
 game_list = list()
 player_list = list()
@@ -15,7 +15,7 @@ for (i in 1:length(years)) {
 }
 
 # combine the list into a single data frame
-games_df <- bind_rows(list)
+games_df <- bind_rows(game_list)
 players_df <- bind_rows(player_list)
 
 # save data to csv
@@ -25,5 +25,27 @@ write_csv(players_df, "player_stats.csv")
 # build an ml to predict next seasons results
 fixtures <- fetch_fixtures(season = 2024)
 
+game_results <- read_csv("v3/data/game_results.csv")
+
+ladder_status <- list()
+
+unique <- game_results %>% distinct(game_results['round.year'], game_results['round.roundNumber'])
+
+for (i in 1:nrow(unique)){
+  year <- as.integer(unique[i,'round.year'])
+  roundnum <- as.integer(unique[i,'round.roundNumber'])
+  tryCatch(
+    {
+    ladder_status[[i]] <- fetch_ladder(season=year, round=roundnum, source='afltables')
+    },
+    error = function(e){
+      ladder_status[[i]] <- "finals"
+    }
+  )
+  }
+
+ladder_df <- bind_rows(ladder_status)
+write.csv(ladder_df, "ladder_stats.csv")
+
 # train the model based on the home team, away team, home ground and result
-model <- lm(result ~ home_team + away_team + home_ground, data = games_df)
+# model <- lm(result ~ home_team + away_team + home_ground, data = games_df)
